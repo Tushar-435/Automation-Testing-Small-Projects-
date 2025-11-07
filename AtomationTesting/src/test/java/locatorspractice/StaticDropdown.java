a227e229ac30cdd017ceeeda99ffc7022ea60bb9
Tushar Singh <122785295+Tushar-435@users.noreply.github.com>
2025-11-07 14:51:51 +0530

    Add files via upload

---
+++ AtomationTesting/src/test/java/locatorspractice/StaticDropdown.java
@@ -0,0 +1,26 @@
+package locatorspractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C://Users/user/Desktop/Automation/chromedriver.exe");
+		
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		WebElement staticdropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+
+		
+		Select dropdown =new Select(staticdropdown);
+		dropdown.selectByIndex(2);
+	}
+
+}
